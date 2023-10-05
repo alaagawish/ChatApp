930f845df816af708642c4b616991675760f9f7a
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-10-05 23:49:58 +0300

    creating database

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextFIeld: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let reference = Database.database().reference()
+        let rooms = reference.child("roomsTest")
     }
 
-
+    @IBAction func signIn(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+        
+        
+    }
 }
 
